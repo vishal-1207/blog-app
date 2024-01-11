d8b7d0b9c71e329c7aa1fb4f4be142a86c50d579
Sahil pawar <63465844+sahilpawar58@users.noreply.github.com>
2024-01-11 20:50:38 +0530

    working multi step form, without proper design

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,13 @@
 let multiStepForm = document.querySelector("[data-multi-step]");
 let multiSteps = [...multiStepForm.querySelectorAll("[data-step]")];
+let cir_num = [...document.getElementsByClassName("cir-num")];
 let currentpage;
 
 currentpage = multiSteps.findIndex(step => {
     return step.classList.contains("active");
 })
+
+cir_num[0].setAttribute("style", "background-color:grey;");
 
 if(currentpage < 0){
     currentpage=0;
@@ -12,10 +15,12 @@ if(currentpage < 0){
 }
 
 multiStepForm.addEventListener("click",e => {
-    console.log(e.target);
+    if(e.target.tagName == "INPUT"){
+        // console.log(e.target);
     let increment;
 
     if(e.target.matches("[data-next]")){
+        // console.log(e.target.tagName);
         increment=1;
     }else if(e.target.matches("[data-previous]")){
         increment=-1;
@@ -26,9 +31,18 @@ multiStepForm.addEventListener("click",e => {
     if(increment == null || currentpage>2 || currentpage<0) {
         return
     }
+    console.log(currentpage);
     multiSteps.forEach((step,index) => {
+        if(currentpage === index){
+            cir_num[index].setAttribute("style","background-color:grey;");
+            console.log(index);
+        }else{
+            cir_num[index].removeAttribute("style");
+        }
+        
         step.classList.toggle("active",currentpage === index)
     })
+    }
 })
 
 function showCurrentStep(){
